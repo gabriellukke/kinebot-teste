ef84df357e523ce46053996c13b544cc4d5911f1
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-01-24 16:33:32 -0300

    final code

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
 
+import users from "../data/users.js";
+
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
+
+app.get("/api/users", (req, res) => {
+
+  console.log(users);
+  res.status(200).json(users.user);
+});
 
 app.post("/api/users", (req, res, next) => {
   res.status(200).send([]);
--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,58 @@
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [heroes, setHeroes] = useState([]);
+  const [heroIndex, setHeroIndex] = useState(0);
+
+  const renderHeroes = async () => {
+    if (!data.length) {
+      return null;
+    }
+
+    if (heroIndex < data.length) {
+      setHeroes((prevState) => [...prevState, data[heroIndex]]);
+      setHeroIndex((prevState) => prevState + 1);
+    }
+  };
+
+  useEffect(() => {
+    const getHeroes = async () => {
+      const response = await fetch('http://localhost:4000/api/users');
+      const data = await response.json();
+      setData(data);
+    };
+    getHeroes();
+  }, []);
+
+  const isButtonDisabled = heroIndex < data.length;
+
   return (
     <div className="App">
-      <h2>reimagined-spork</h2>
+      <header className="header">
+        <h2>reimagined-spork</h2>
+      </header>
+      <main>
+        <div className="form">
+          <button
+            type="button"
+            onClick={() => renderHeroes()}
+            disabled={!isButtonDisabled}
+          >
+            click
+          </button>
+        </div>
+        <section className="heroes-container">
+          {!!heroes.length &&
+            heroes.map((hero) => (
+              <div key={hero.id} className="hero-card">
+                <h2>{hero.name}</h2>
+                <img alt={`${hero.name} hero`} src={hero.image} />
+              </div>
+            ))}
+        </section>
+      </main>
     </div>
   );
 }
